1799c8181129b5dc43db135d4ebbff2c0d60f12f
AMiner <aminer@AMinerdeMacBook-Pro.local>
2020-11-06 18:18:42 +0800

    update

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from "expo-status-bar"
 import React from "react"
-import Navigation from './navigation';
-import { View } from "react-native"
+import Navigation from "./navigation"
+import { View, Text } from "react-native"
+import { SafeAreaProvider } from "react-native-safe-area-context"
 
 export default function App() {
   return (
-    <View>
-      <Navigation/>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+      </View>
+      <Navigation />
+      <StatusBar />
+    </SafeAreaProvider>
   )
 }
---
+++ navigation/index.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { View, Text } from "react-native"
+// import { StackNavigator } from "react-navigation"
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const HomeScreen= () => (
+  <View>
+    <Text>Home Screen</Text>
+  </View>
+)
+
+const DetailsScreen = () => (
+  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <Text>Details Screen</Text>
+  </View>
+)
+
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator  screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
